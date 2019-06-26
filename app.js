3902ea5ee2d9953042dc904e3aaef2103acb5b0e
Tony <tony.n.flpa@gmail.com>
2019-06-26 14:38:30 -0400

    yeeet

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, function(err){
+
+
+app.set("view engine","ejs");
+
+app.get('/home', function (req, res) {
+    res.render('home');
+  });
+
+  app.listen(3000, function(err){
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')

a1234abf2f95b710fdabacd56d6fc3d13104b6c0
Roger <idevelopedreams@gmail.com>
2019-06-27 14:42:41 -0400

    form is accepting task items with the help of body parser

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({extended: false})
+
+const dummyData = [];
+
+// setting up
 const app = express();
 
 // setting template engine
@@ -7,11 +13,24 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
+
+
+// ############### ROUTES ##############
+
+// Get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
     res.render('tasks');
-  });
+});
 
-  app.listen(3000, function(err){
+// Post for tasks: posting a task
+app.post('/tasks', urlEncoded, function(req, res){
+
+  dummyData.push(req.body.task)
+  console.log(dummyData)
+
+});
+
+app.listen(3000, function(err){
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')

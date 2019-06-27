d228d9f63c83ba78d1c437afdb793d374f4a1726
Roger <idevelopedreams@gmail.com>
2019-06-27 13:05:10 -0400

    server hadling request for static files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 app.set("view engine","ejs");
+
+// use middle ware to serve static files
+app.use(express.static('./public'));
 
 app.get('/tasks', function (req, res) {
     res.render('tasks');

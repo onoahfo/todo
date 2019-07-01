f9204b855cfdd55ffba65e6b20e55ae733c2198d
Roger <idevelopedreams@gmail.com>
2019-07-01 11:28:51 -0400

    serving task from server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
 
-const dummyData = [];
+const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime"},{taskItem: "Sleep"}];
 
 // setting up
 const app = express();
@@ -19,15 +19,16 @@ app.use(express.static('./public'));
 
 // Get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
-    res.render('tasks');
+    res.render('tasks', {taskToDo: dummyData});
 });
 
 // Post for tasks: posting a task
 app.post('/tasks', urlEncoded, function(req, res){
-
-  dummyData.push(req.body.task)
+  let incomingItem = {}
+  incomingItem.taskItem = req.body.task
+  dummyData.push(incomingItem)
   console.log(dummyData)
-
+  res.redirect('/tasks')
 });
 
 app.listen(3000, function(err){
